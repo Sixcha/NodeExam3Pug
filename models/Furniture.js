af5e6ad28f9e9da64f1372307686b66ed8fb26e9
Paul Bora <paulbora_akcan@hotmail.com>
2024-07-12 00:29:11 +0200

    -Add quantity to Furniture model

--- models/Furniture.js
+++ models/Furniture.js
@@ -4,7 +4,7 @@ const FurnitureSchema = new mongoose.Schema({
     name: String,
     category: String,
     materials: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Material' }],
-    keywords: [String]
+    quantity: Number
 });
 
 module.exports = mongoose.model('Furniture', FurnitureSchema);
